7604124962f70a85555f4dd65a1b680aee78ebc4
shadawo <ribe.thibaut@gmail.com>
2022-02-01 21:41:07 +0100

    changementDePage

---
+++ app/src/main/java/fr/isen/ribe/androiderestaurant/DishesActivity.kt
@@ -0,0 +1,11 @@
+package fr.isen.ribe.androiderestaurant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DishesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dishes)
+    }
+}
--- app/src/main/java/fr/isen/ribe/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/ribe/androiderestaurant/HomeActivity.kt
@@ -1,11 +1,39 @@
 package fr.isen.ribe.androiderestaurant
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import fr.isen.ribe.androiderestaurant.databinding.ActivityHomeBinding
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.starters.setOnClickListener {
+            changeActivity(getString(R.string.home_starters))
+        }
+        binding.dishes.setOnClickListener {
+            changeActivity(getString(R.string.home_dish))
+        }
+        binding.desserts.setOnClickListener {
+            changeActivity(getString(R.string.home_dessert))
+        }
     }
+
+    private fun changeActivity(category: String) {
+        val changePage: Intent = Intent(this, DishesActivity::class.java)
+        changePage.putExtra("category_type",category)
+        Log.i("INFO","End of HomeActivity")
+        startActivity(changePage)
+    }
+
+
 }
